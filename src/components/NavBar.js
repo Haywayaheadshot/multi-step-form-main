81ae47fb0a935696d9b5a9024d2c447db65a24fd
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-08 01:35:24 -0600

    Add active class names to Navlinks to give buttons a background when active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,22 +6,22 @@ export default function NavBar() {
   return (
     <div className="nav-bar-container">
       <ul className="nav-bar-ul">
-        <NavLink className="desktop-ul-li" to="/">
+        <NavLink className="desktop-ul-li" to="/" activeclassname="active-link">
           <li className="nav-bar-ul-li">
             <button className="nav-bar-btns font" type="button">1</button>
           </li>
         </NavLink>
-        <NavLink className="desktop-ul-li" to="/secondPage">
+        <NavLink className="desktop-ul-li" to="/secondPage" activeclassname="active-link">
           <li className="nav-bar-ul-li">
             <button className="nav-bar-btns font" type="button">2</button>
           </li>
         </NavLink>
-        <NavLink className="desktop-ul-li" to="/thirdPage">
+        <NavLink className="desktop-ul-li" to="/thirdPage" activeclassname="active-link">
           <li className="nav-bar-ul-li">
             <button className="nav-bar-btns font" type="button">3</button>
           </li>
         </NavLink>
-        <NavLink className="desktop-ul-li" to="/fourthPage">
+        <NavLink className="desktop-ul-li" to="/fourthPage" activeclassname="active-link">
           <li className="nav-bar-ul-li">
             <button className="nav-bar-btns font" type="button">4</button>
           </li>
