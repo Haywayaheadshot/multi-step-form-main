56bf357a972d629f0ad95a5f83b661cfbfbdf40e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-10 07:08:12 -0600

    Create switch component to toggle between monthly and yearly

--- src/components/dep/Switch.js
+++ src/components/dep/Switch.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/switch.css';
 
 function Switch() {
+  const [checked, setChecked] = useState({
+    checked: false,
+    setChecked: true,
+  });
   return (
-    <div>Switch</div>
+    <div>
+      <label htmlFor="switch" className="switch-label">
+        <input className="switch-input" id="switch" type="checkbox" />
+        <span className="switch-slider" />
+      </label>
+    </div>
   );
 }
 
