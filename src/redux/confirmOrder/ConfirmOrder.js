73c0f1f31f18c8a3f1fee4022f4c113e437d8e15
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-16 07:53:29 -0600

    Write action and reducer for plan

---
+++ src/redux/confirmOrder/ConfirmOrder.js
@@ -0,0 +1,30 @@
+// Write action and reducer to handle plans
+
+const ADD_PLAN = 'MSFM/src/redux/confirmOrder/addPlan';
+// const ADD_ADD_ONS = 'MSFM/src/redux/confirmOrder/addAddOns';
+// const CHANGE_PLAN = 'book-keeper/src/redux/confirmOrder/changePlan';
+
+// First try with hard coded plan
+const defaultPlanState = [
+  {
+    id: 1,
+    plan: 'Arcade',
+  },
+];
+
+export default function planReducer(state = defaultPlanState, action) {
+  switch (action.type) {
+    case ADD_PLAN:
+      return [
+        ...state,
+        {
+          id: action.id,
+          plan: action.plan,
+        },
+      ];
+    //   case REMOVE_BOOK:
+    //     return state.filter((book) => book.id !== action.id);
+    //   default:
+    //     return state;
+  }
+}
