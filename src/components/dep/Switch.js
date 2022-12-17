61de5699c4cd140f330db0850da8dfee6ad73176
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-17 11:54:15 -0600

    Use day and night state to toggle light and dark mode

--- src/components/dep/Switch.js
+++ src/components/dep/Switch.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/App.css';
 import '../../styles/switch.css';
 
 function Switch() {
+  const [day, night] = useState(false);
+  const { body } = document;
   return (
     <div>
-      <label htmlFor="switch" className="switch-label">
-        <input className="switch-input" id="switch" type="checkbox" />
-        <span className="switch-slider" />
+      <label htmlFor="switch-app" className="switch-label-app">
+        {!day && (
+          body.classList.add('day'),
+          body.classList.remove('night')
+        )}
+        {day && (
+          body.classList.replace('day', 'night')
+        )}
+        <input onClick={() => night((day) => !day)} className="switch-input-app" id="switch-app" type="checkbox" />
+        <span className="switch-slider-app" />
       </label>
     </div>
   );
