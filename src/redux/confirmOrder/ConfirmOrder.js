3f7f68aa76a83c1da3243a3d6834eb4506be065e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-23 21:10:00 -0600

    Empty state anytime the user choses a different item before updating

--- src/redux/confirmOrder/ConfirmOrder.js
+++ src/redux/confirmOrder/ConfirmOrder.js
@@ -1,32 +1,41 @@
 // Write action and reducer to handle plans
 
 const ADD_PLAN = 'MSFM/src/redux/confirmOrder/addPlan';
-// const ADD_ADD_ONS = 'MSFM/src/redux/confirmOrder/addAddOns';
-// const CHANGE_PLAN = 'book-keeper/src/redux/confirmOrder/changePlan';
+const REMOVE_PLAN = 'book-keeper/src/redux/confirmOrder/removePlan';
 
 // First try with hard coded plan
-const defaultPlanState = [
-  {
-    id: 1,
-    plan: 'Arcade',
-    amount: '9',
-  },
-];
+const defaultPlanState = [];
+
+// action creators for add and remove plans
+
+export const addPlan = (payload) => ({
+  type: ADD_PLAN,
+  id: payload.id,
+  plan: payload.plan,
+  amount: payload.amount,
+});
+
+export const removePlan = (payload) => ({
+  type: REMOVE_PLAN,
+  id: payload.id,
+});
 
 export default function planReducer(state = defaultPlanState, action) {
   switch (action.type) {
     default: return state;
     case ADD_PLAN:
+      state.splice(0, state.length);
       return [
         ...state,
         {
           id: action.id,
           plan: action.plan,
+          amount: action.amount,
         },
       ];
-    //   case REMOVE_BOOK:
-    //     return state.filter((book) => book.id !== action.id);
-    //   default:
-    //     return state;
+    case REMOVE_PLAN:
+      return [
+        state.filter((plan) => plan.id !== action.id),
+      ];
   }
 }
