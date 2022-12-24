dde202c6b97faec869c4b29fdb18ed47bb0213a0
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-23 21:05:35 -0600

    Add addOns redux folder

---
+++ src/redux/addOns/AddOns.js
@@ -0,0 +1,39 @@
+// Write action and reducer to handle plans
+
+const ADD_ADD_ONS = 'MSFM/src/redux/confirmOrder/addAddOns';
+const REMOVE_ADD_ONS = 'MSFM/src/redux/confirmOrder/removeAddOns';
+
+const defaultAddOnsState = [];
+
+// action creators for add and remove addOns
+
+export const addAddOns = (payload) => ({
+  type: ADD_ADD_ONS,
+  id: payload.id,
+  name: payload.name,
+  amount: payload.amount,
+});
+
+export const removeAddOns = (payload) => ({
+  type: REMOVE_ADD_ONS,
+  id: payload.id,
+});
+
+export default function addOnsReducer(state = defaultAddOnsState, action) {
+  switch (action.type) {
+    default: return state;
+    case ADD_ADD_ONS:
+      return [
+        ...state,
+        {
+          id: action.id,
+          name: action.name,
+          amount: action.amount,
+        },
+      ];
+    case REMOVE_ADD_ONS:
+      return [
+        state.filter((addOn) => addOn.id !== action.id),
+      ];
+  }
+}
