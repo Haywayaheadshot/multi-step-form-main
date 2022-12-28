6ec89646aed3b225cb381b94803315d96d7363b8
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-28 15:59:12 +0000

    Add chosen as prop and use to check input

--- src/pages/ThirdPage.js
+++ src/pages/ThirdPage.js
@@ -1,67 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { addAddOns, removeAddOns } from '../redux/addOns/AddOns';
 import '../styles/third-page.css';
 
-export default function ThirdPage() {
-  const [chosen, setChosen] = useState(false);
-  const [chosen1, setChosen1] = useState(false);
-  const [chosen2, setChosen2] = useState(false);
-
+export default function ThirdPage({ chosen, chosen1, chosen2 }) {
   const dispatch = useDispatch();
 
-  const handleSelectedAddOn1 = (key) => {
-    setChosen1((chosen) => !chosen);
+  const handleSelectedAddOn1 = () => {
+    dispatch(addAddOns({
+      id: 'add-on-one',
+      name: 'Online service',
+      amount: '1',
+    }, []));
+  };
 
-    if ((key.target.value === 'unchecked')) {
-      dispatch(addAddOns({
-        id: 'add-on-two',
-        name: 'Larger Storage',
-        amount: '2',
-        value: true,
-      }));
-    } else if (key.target.value === 'on') { // Testing just with addon 2, we have a problem.
+  const removeSelectedAddOn1 = () => {
+    if (!chosen) {
       dispatch(removeAddOns({
-        id: 'add-on-two',
-      }));
-    }
-  };
-
-  const handleSelectedAddOn2 = (key) => {
-    setChosen2((chosen) => !chosen);
-
-    if ((key.target.value === 'unchecked')) {
-      dispatch(addAddOns({
-        id: 'add-on-three',
-        name: 'Customizable Profile',
-        amount: '2',
-        value: true,
-      }));
-    }
-    // else if (key.target.value === 'on') {
-    //   dispatch(removeAddOns({
-    //     id: 'add-on-three',
-    //   }));
-    // }
-  };
-
-  const handleSelectedAddOn = (key) => {
-    setChosen((chosen) => !chosen);
-
-    if ((key.target.value === 'unchecked')) {
-      dispatch(addAddOns({
         id: 'add-on-one',
-        name: 'Online Service',
-        amount: '1',
-        value: true,
       }));
     }
-    // else if (key.target.value === 'on') {
-    //   dispatch(removeAddOns({
-    //     id: 'add-on-one',
-    //   }));
-    // }
+  };
+
+  const handleSelectedAddOn2 = () => {
+    console.log(key)
+    dispatch(addAddOns({
+      id: 'add-on-two',
+      name: 'Larger Storage',
+      amount: '2',
+    }));
+  };
+
+  const removeSelectedAddOn2 = () => {
+    dispatch(removeAddOns({
+      id: 'add-on-two',
+    }));
+  };
+
+  const handleSelectedAddOn3 = () => {
+    dispatch(addAddOns({
+      id: 'add-on-three',
+      name: 'Customizable Profile',
+      amount: '2',
+    }));
+  };
+
+  const removeSelectedAddOn3 = () => {
+    dispatch(removeAddOns({
+      id: 'add-on-three',
+    }));
   };
 
   return (
@@ -72,9 +61,11 @@ export default function ThirdPage() {
           Add-ons help enhance your gaming experience.
         </p>
         <div className="add-ons-container">
-          <section className="add-ons-child" key="add-on-one" onClick={handleSelectedAddOn} role="presentation">
-            { chosen && (<input className="third-page-checkBox" id="add-on-one" type="checkbox" defaultChecked />) }
-            { !chosen && (<input className="third-page-checkBox" id="add-on-one" type="checkbox" value="unchecked" />) }
+          <section className="add-ons-child" key="add-on-one">
+            <>
+              { !chosen && (<input className="third-page-checkBox" onClick={handleSelectedAddOn1} type="checkbox" />)}
+              { chosen && (<input className="third-page-checkBox" onClick={removeSelectedAddOn1} type="checkbox" />)}
+            </>
             <div className="add-ons-package">
               <h2 className="add-ons-package-h2 font">Online service</h2>
               <h4 className="add-ons-package-h4 font">Access to multiplayer games</h4>
@@ -83,9 +74,9 @@ export default function ThirdPage() {
               <h4 className="monthly-addOns font">+$1/mo</h4>
             </div>
           </section>
-          <section className="add-ons-child" key="add-on-two" onClick={handleSelectedAddOn1} role="presentation">
-            { chosen1 && (<input className="third-page-checkBox" id="add-on-two" type="checkbox" defaultChecked />) }
-            { !chosen1 && (<input className="third-page-checkBox" id="add-on-two" type="checkbox" value="unchecked" />) }
+          <section className="add-ons-child" key="add-on-two">
+            { !chosen1 && (<input className="third-page-checkBox" id="add-on-two-true" key="add-on-two-true" onClick={handleSelectedAddOn2} type="checkbox" />)}
+            { chosen1 && (<input id="add-on-two-false" key="add-on-two-false" onClick={removeSelectedAddOn2} type="checkbox" />)}
             <div className="add-ons-package">
               <h2 className="add-ons-package-h2 font">Larger storage</h2>
               <h4 className="add-ons-package-h4 font">Extra 1TB of cloud save</h4>
@@ -94,9 +85,11 @@ export default function ThirdPage() {
               <h4 className="monthly-addOns font">+$2/mo</h4>
             </div>
           </section>
-          <section className="add-ons-child" key="add-on-three" onClick={handleSelectedAddOn2} role="presentation">
-            { chosen2 && (<input className="third-page-checkBox" id="add-on-three" type="checkbox" defaultChecked />) }
-            { !chosen2 && (<input className="third-page-checkBox" id="add-on-three" type="checkbox" value="unchecked" />) }
+          <section className="add-ons-child" key="add-on-three">
+            {/* <input className="third-page-checkBox" key="add-on-three"
+            type="checkbox" defaultChecked /> */}
+            { !chosen2 && (<input className="third-page-checkBox" id="add-on-three-true" key="add-on-three-true" onClick={handleSelectedAddOn3} type="checkbox" />)}
+            { chosen2 && (<input className="third-page-checkBox" id="add-on-three-false" key="add-on-three-false" onClick={removeSelectedAddOn3} type="checkbox" />)}
             <div className="add-ons-package">
               <h2 className="add-ons-package-h2 font">Customizable Profile</h2>
               <h4 className="add-ons-package-h4 font">Custom theme on your profile</h4>
@@ -111,10 +104,16 @@ export default function ThirdPage() {
         <NavLink to="/secondPage">
           <button className="sp-go-back-btn font" type="button">Go Back</button>
         </NavLink>
-        <NavLink to="/fourthPage">
+        <NavLink to="/fourthPage" key="third-page-next-btn">
           <button className="fp-next-page-btn font" type="button">Next Step</button>
         </NavLink>
       </section>
     </div>
   );
 }
+
+ThirdPage.propTypes = {
+  chosen: PropTypes.bool.isRequired,
+  chosen1: PropTypes.bool.isRequired,
+  chosen2: PropTypes.bool.isRequired,
+};
